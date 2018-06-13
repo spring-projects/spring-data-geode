89cafeba2d1fbb141eec5886dcb7f541f0370b62
John Blum <jblum@pivotal.io>
2018-06-12 19:42:55 -0700

    DATAGEODE-122 - Refactor Function annotation configuration to be smarter in resolving Apache Geode objects on startup.
    Renames OnMember, OnMembers, OnRegion, OnServer and OnServers *ExecutionBeanDefinitionBuilder classes to *FunctionExecutionBeanDefinitionBuilder.

--- src/main/java/org/springframework/data/gemfire/function/config/AbstractFunctionExecutionBeanDefinitionBuilder.java
+++ src/main/java/org/springframework/data/gemfire/function/config/AbstractFunctionExecutionBeanDefinitionBuilder.java
@@ -1,16 +1,19 @@
 /*
  * Copyright 2002-2013 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
+
 package org.springframework.data.gemfire.function.config;
+
+import java.util.Optional;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -35,40 +38,37 @@ abstract class AbstractFunctionExecutionBeanDefinitionBuilder {
 	protected final Log log = LogFactory.getLog(getClass());
 
 	/**
-	 * 
 	 * @param configuration the configuration values
 	 */
-	 AbstractFunctionExecutionBeanDefinitionBuilder(FunctionExecutionConfiguration configuration) {
-		Assert.notNull(configuration);
+	AbstractFunctionExecutionBeanDefinitionBuilder(FunctionExecutionConfiguration configuration) {
+		Assert.notNull(configuration, "Configuration must not be null");
 		this.configuration = configuration;
 	}
-	
+
 	/**
 	 * Build the bean definition
-	 * @param registry
-	 * @return
 	 */
-	 BeanDefinition build(BeanDefinitionRegistry registry) {
+	BeanDefinition build(BeanDefinitionRegistry registry) {
 		BeanDefinitionBuilder functionProxyFactoryBeanBuilder = BeanDefinitionBuilder.rootBeanDefinition(
 			getFunctionProxyFactoryBeanClass());
 
-		functionProxyFactoryBeanBuilder.addConstructorArgValue(configuration.getFunctionExecutionInterface());
+		functionProxyFactoryBeanBuilder.addConstructorArgValue(this.configuration.getFunctionExecutionInterface());
 		functionProxyFactoryBeanBuilder.addConstructorArgReference(BeanDefinitionReaderUtils.registerWithGeneratedName(
 			buildGemfireFunctionOperations(registry), registry));
-		 
+
 		return functionProxyFactoryBeanBuilder.getBeanDefinition();
-	}	
+	}
 
 	protected AbstractBeanDefinition buildGemfireFunctionOperations(BeanDefinitionRegistry registry) {
+
 		BeanDefinitionBuilder functionTemplateBuilder = getGemfireFunctionOperationsBeanDefinitionBuilder(registry);
 
 		functionTemplateBuilder.setLazyInit(true);
 
-		String resultCollectorReference = (String) configuration.getAttribute("resultCollector");
-
-		if (StringUtils.hasText(resultCollectorReference)){
-			functionTemplateBuilder.addPropertyReference("resultCollector", resultCollectorReference);
-		}
+		Optional.ofNullable(this.configuration.getAttribute("resultCollector"))
+			.map(String::valueOf)
+			.filter(StringUtils::hasText)
+			.ifPresent(reference -> functionTemplateBuilder.addPropertyReference("resultCollector", reference));
 
 		return functionTemplateBuilder.getBeanDefinition();
 	}
--- src/main/java/org/springframework/data/gemfire/function/execution/GemfireOnRegionFunctionTemplate.java
+++ src/main/java/org/springframework/data/gemfire/function/execution/GemfireOnRegionFunctionTemplate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2013 the original author or authors.
+ * Copyright 2002-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -10,6 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
+
 package org.springframework.data.gemfire.function.execution;
 
 import java.util.Set;
@@ -20,50 +21,58 @@ import org.springframework.util.Assert;
 
 /**
  * @author David Turanski
- *
+ * @author John Blum
  */
 public class GemfireOnRegionFunctionTemplate extends AbstractFunctionTemplate implements GemfireOnRegionOperations {
 
-	private Region<?, ?> region;
+	private final Region<?, ?> region;
 
 	/**
-	 * Constructs an instance of the GemFireOnRegionFunctionTemplate with the given GemFire Cache Region.
+	 * Constructs a new instance of the {@link GemfireOnRegionFunctionTemplate} initialized with
+	 * the given {@link Region}.
 	 *
-	 * @param region the GemFire Cache Region upon which the Function will be executed.
+	 * @param region the {@link Region} upon which the {@link Function} will be executed.
+	 * @throws IllegalArgumentException if {@link Region} is {@literal null}.
 	 * @see org.apache.geode.cache.Region
 	 */
 	public GemfireOnRegionFunctionTemplate(Region<?, ?> region) {
-		Assert.notNull(region, "Region cannot be null");
+
+		Assert.notNull(region, "Region must not be null");
+
 		this.region = region;
 	}
 
 	@Override
-	public <T> Iterable<T> execute(Function function, Set<?> keys, Object... args) {
-		return execute(new RegionFunctionExecution(region).setKeys(keys).setFunction(function).setTimeout(timeout)
-				.setArgs(args));
-	}
-
-	@Override
-	public <T> Iterable<T> execute(String functionId, Set<?> keys, Object... args) {
-		return execute(new RegionFunctionExecution(region).setKeys(keys).setFunctionId(functionId).setTimeout(timeout)
-				.setArgs(args));
-	}
-
-	@Override
-	public <T> T executeAndextract(String functionId, Set<?> keys, Object... args) {
-		return this.<T> executeAndExtract(new RegionFunctionExecution(region).setKeys(keys).setFunctionId(functionId)
-				.setTimeout(timeout).setArgs(args));
-	}
-
-	@Override
-	protected AbstractFunctionExecution getFunctionExecution() {
+	protected RegionFunctionExecution getFunctionExecution() {
 		return new RegionFunctionExecution(this.region);
 	}
 
 	@Override
-	public void executeWithNoResult(String functionId, Set<?> keys, Object... args) {
-		execute(new RegionFunctionExecution(region).setKeys(keys).setFunctionId(functionId).setTimeout(timeout)
-				.setArgs(args), false);
+	public <T> Iterable<T> execute(String functionId, Set<?> keys, Object... args) {
+
+		return execute(getFunctionExecution()
+			.setKeys(keys)
+			.setFunctionId(functionId)
+			.setTimeout(this.timeout)
+			.setArgs(args));
 	}
 
+	@Override
+	public <T> T executeAndExtract(String functionId, Set<?> keys, Object... args) {
+
+		return executeAndExtract(getFunctionExecution()
+			.setKeys(keys)
+			.setFunctionId(functionId)
+			.setTimeout(this.timeout).setArgs(args));
+	}
+
+	@Override
+	public void executeWithNoResult(String functionId, Set<?> keys, Object... args) {
+
+		execute(getFunctionExecution()
+			.setKeys(keys)
+			.setFunctionId(functionId)
+			.setTimeout(this.timeout)
+			.setArgs(args), false);
+	}
 }
