bfe0a6c93d6fef0149a23a432a0504bb71f21007
Patrick Johnson <pjohnson@pivotal.io>
2020-02-10 13:07:03 -0800

    Enable Function Executions to return results from all servers.
    Refer to DATAGEODE-295.
    
    Resolves gh-37.

--- spring-data-geode/src/test/java/org/springframework/data/gemfire/function/execution/onservers/AllServersAdminFunctions.java
+++ spring-data-geode/src/test/java/org/springframework/data/gemfire/function/execution/onservers/AllServersAdminFunctions.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2021 the original author or authors.
+ * Copyright 2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,10 +16,10 @@
  */
 package org.springframework.data.gemfire.function.execution.onservers;
 
+import java.util.List;
+
 import org.springframework.data.gemfire.function.annotation.FunctionId;
 import org.springframework.data.gemfire.function.annotation.OnServers;
-
-import java.util.List;
 
 /**
  * @author Patrick Johnson
@@ -29,4 +29,5 @@ public interface AllServersAdminFunctions {
 
 	@FunctionId("GetAllMetricsFunction")
 	List<List<Metric>> getAllMetrics();
+
 }
