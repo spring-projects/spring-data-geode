ece4ea9e97247d6d3f88c9bba632257d756ef3ed
John Blum <jblum@vmware.com>
2021-09-27 11:24:43 -0700

    Reinstate OnMemberFunctionExecutionBeanDefinitionBuilder class, which was removed after rebasing on the latest changes from main.

--- spring-data-geode/src/main/java/org/springframework/data/gemfire/function/config/OnMemberFunctionExecutionBeanDefinitionBuilder.java
+++ spring-data-geode/src/main/java/org/springframework/data/gemfire/function/config/OnMemberFunctionExecutionBeanDefinitionBuilder.java
@@ -10,7 +10,6 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-
 package org.springframework.data.gemfire.function.config;
 
 import org.springframework.data.gemfire.function.execution.GemfireOnMemberFunctionTemplate;
@@ -25,10 +24,6 @@ class OnMemberFunctionExecutionBeanDefinitionBuilder extends MemberBasedFunction
 		super(configuration);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.springframework.data.gemfire.function.config.MemberBasedFunctionExecutionBeanDefinitionBuilder#getGemfireFunctionOperationsClass()
-	 */
 	@Override
 	protected Class<?> getGemfireOperationsClass() {
 		return GemfireOnMemberFunctionTemplate.class;
