896611ba3edf8d192b0b738f8270d08b3652174b
John Blum <jblum@vmware.com>
2022-01-20 13:47:47 -0800

    Replace Java EE 9 (javax) packages with Jakarta EE (jakarta) packages.

--- spring-data-geode/src/test/java/org/springframework/data/gemfire/config/annotation/EntityDefinedRegionsForMockCollocatedPartitionRegionsIntegrationsTests.java
+++ spring-data-geode/src/test/java/org/springframework/data/gemfire/config/annotation/EntityDefinedRegionsForMockCollocatedPartitionRegionsIntegrationsTests.java
@@ -18,7 +18,7 @@ package org.springframework.data.gemfire.config.annotation;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
-import javax.annotation.Resource;
+import jakarta.annotation.Resource;
 
 import org.junit.Before;
 import org.junit.Test;
--- spring-data-geode/src/test/java/org/springframework/data/gemfire/config/annotation/EntityDefinedRegionsForRealCollocatedPartitionRegionsIntegrationTests.java
+++ spring-data-geode/src/test/java/org/springframework/data/gemfire/config/annotation/EntityDefinedRegionsForRealCollocatedPartitionRegionsIntegrationTests.java
@@ -17,7 +17,7 @@ package org.springframework.data.gemfire.config.annotation;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import javax.annotation.Resource;
+import jakarta.annotation.Resource;
 
 import org.junit.Before;
 import org.junit.Test;
